59eea8766f43b1edac7054704792945feba74938
Zeeshan Abid <zeeshan595@tutanota.com>
2017-10-03 10:13:26 +0100

    tutanota-release v2.15

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
 if (typeof importScripts !== 'function') {
-    tutao.env = new tutao.Environment(tutao.Env.PROD, true, 'app.tutanota.com', null, 'https://pay.tutanota.com', '2.14.7');
+    tutao.env = new tutao.Environment(tutao.Env.PROD, true, 'app.tutanota.com', null, 'https://pay.tutanota.com', '2.15.0');
     tutao.tutanota.Bootstrap.init();
 }
